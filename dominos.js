80bceb2604078f8fbd233a4977a92706fe65946e
FOUAD EL MIR <46565680+Fouad7889@users.noreply.github.com>
2019-01-22 13:11:13 +0100

    Jeu de Dominos
    Mes touts premiers pas dans la conception d'un jeu de Dominos

---
+++ dominos.js
@@ -0,0 +1,16 @@
+/*console.log(document.getElementById('boutonTest'));
+console.log(document.getElementsByClassName('boutons'));
+
+document.getElementById('boutonTest').addEventListener('click', function () {
+    console.log('aEteClické')
+})
+
+function ecritAEteClicker() {
+    console.log('aEteClické');
+
+}*/
+
+var userName = prompt('Entrez votre prénom :');
+alert(userName); // Affiche le prénom entré par l'utilisateur
+
+
